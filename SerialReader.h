600303f3d0e2dc0f55837cb314352f348b252cf5
Will Scheirey <williamscheirey@gmail.com>
2024-02-28 17:11:17 -0500

    basic serial reader class

---
+++ SerialReader.h
@@ -0,0 +1,47 @@
+//
+// Created by William Scheirey on 2/28/24.
+//
+
+#ifndef XBEE_BACKEND_CPP_SERIALREADER_H
+#define XBEE_BACKEND_CPP_SERIALREADER_H
+
+#include <QObject>
+#include <QtSerialPort//QtSerialPort>
+
+
+class SerialReader: public QObject
+{
+    Q_OBJECT
+
+public:
+    SerialReader(const QSerialPortInfo& port, int baudRate, QObject *parent = nullptr);
+
+private:
+    int m_baudRate;
+    QSerialPort *m_serialPort;
+    QSerialPortInfo portInfo;
+
+    void connectSignals();
+
+public slots:
+    void    baudRateChanged(qint32 baudRate, QSerialPort::Directions directions);
+    void	breakEnabledChanged(bool set);
+    void	dataBitsChanged(QSerialPort::DataBits dataBits);
+    void	dataTerminalReadyChanged(bool set);
+    void	errorOccurred(QSerialPort::SerialPortError error);
+    void	flowControlChanged(QSerialPort::FlowControl flow);
+    void	parityChanged(QSerialPort::Parity parity);
+    void	requestToSendChanged(bool set);
+    void	stopBitsChanged(QSerialPort::StopBits stopBits);
+
+    void	aboutToClose();
+    void	bytesWritten(qint64 bytes);
+    void	channelBytesWritten(int channel, qint64 bytes);
+    void	channelReadyRead(int channel);
+    void	readChannelFinished();
+    void	readyRead();
+
+};
+
+
+#endif //XBEE_BACKEND_CPP_SERIALREADER_H
--- main.cpp
+++ main.cpp
@@ -1,9 +1,43 @@
 #include <QCoreApplication>
 #include <QDebug>
+#include <iostream>
+
+#include "SerialReader.h"
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    qDebug() << "Hello World";
+
+    QList serialPorts = QSerialPortInfo::availablePorts();
+
+    QSerialPortInfo targetPort;
+
+    std::cout << "Available baud rates: \n";
+    for(auto &baudRate : QSerialPortInfo::standardBaudRates())
+    {
+        std::cout << "\t" << baudRate << "\n";
+    }
+
+    std::cout << "\nFound serial ports: \n";
+    for (auto &port: serialPorts)
+    {
+        std::cout << "\n" << port.portName().toStdString() << "\n";
+        std::cout << "\tManufacturer: " << port.manufacturer().toStdString() << "\n";
+        std::cout << "\tSystem location: " << port.systemLocation().toStdString() << "\n";
+        std::cout << "\tSerial number: " << port.serialNumber().toStdString() << "\n";
+
+        if (!port.portName().startsWith("tty") || port.portName().contains("Bluetooth"))
+            continue;
+    }
+
+    std::cout.flush();
+
+
+
+    SerialReader serialReader(serialPorts.at(1), 9600);
+
+
+//    SerialReader reader();
+
     return QCoreApplication::exec();
 }

01d0a9b36dd0b7092eb5a625e4e5b69f60da3b87
Will Scheirey <williamscheirey@gmail.com>
2024-03-11 17:41:24 -0400

    contains instead of equals

--- main.cpp
+++ main.cpp
@@ -33,7 +33,7 @@ int main(int argc, char *argv[])
         std::cout << "\tSerial number: " << port.serialNumber().toStdString() << "\n";
 #endif
 
-        if (port.portName().startsWith("tty") && port.manufacturer() == "Digi")
+        if (port.portName().startsWith("tty") && port.manufacturer().contains("Digi"))
             targetPort = port;
     }
 
